f866c98cd16bb75edb4ae451611391940d88846b
muff <mansoorroeen71@gmail.com>
2022-09-22 18:01:06 +0530

    changes made

--- pages/blogs/index.tsx
+++ pages/blogs/index.tsx
@@ -2,7 +2,6 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import DefaultLayout from '../../app/components/layouts/Default'
 import BlogsPage from '../../app/components/templates/blogs/Blogs'
-import ProjectsPage from '../../app/components/templates/projects/Projects'
 
 const PageHead = () => {
   return (
